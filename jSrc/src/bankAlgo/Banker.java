8400fb6004ea57155b2904a7d5f4f6b89fafe5fa
dajacobs <dajacobs.tab@gmail.com>
2015-09-24 17:56:45 -0500

    update

--- jSrc/src/bankAlgo/Banker.java
+++ jSrc/src/bankAlgo/Banker.java
@@ -3,11 +3,26 @@ package bankAlgo;
 import java.util.Scanner;
 
 public class Banker {
-    private int need[][];
-    private int allo[][];
-    private int aval[][];
-    private int numP;
-    private int numR;
+    private int min[][];
+    private int max[][];
+    private int allocate[][];
+    private int available[][];
+    private int numbProc;
+    private int numbReso;
     
-    
+    private void input() {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter number of processes and resources: ");
+        numbProc = scan.nextInt();
+        numbReso = scan.nextInt();
+        
+        min = new int[numbProc][numbReso];
+        max = new int[numbProc][numbReso];
+        allocate = new int[numbProc][numbReso];
+        available = new int[1][numbReso];
+        
+        System.out.println("Enter allocate values: ");
+        
+    }
 }
