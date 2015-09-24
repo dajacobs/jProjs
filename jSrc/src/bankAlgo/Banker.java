4e2ff429ec3b977d96263d19f9d5bc2a66440402
dajacobs <dajacobs.tab@gmail.com>
2015-09-24 16:31:00 -0500

    initial

---
+++ jSrc/src/bankAlgo/Banker.java
@@ -0,0 +1,13 @@
+package bankAlgo;
+
+import java.util.Scanner;
+
+public class Banker {
+    private int need[][];
+    private int allo[][];
+    private int aval[][];
+    private int numP;
+    private int numR;
+    
+    
+}
